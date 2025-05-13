238ecf61b217ada4f9bddd3eb877ce4cc854fe90
muhammadQasimRanjha786 <m.qasim.ranjha786@gmail.com>
2025-05-13 12:27:43 +0500

    Made changes in portfolio

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -42,8 +42,8 @@ export default function Greeting() {
                 <Button text="Contact me" href="#contact" />
                 {greeting.resumeLink && (
                   <a
-                    href={require("./resume.pdf")}
-                    download="Resume.pdf"
+                    href={require("./M.Qasim_GHL_CV.pdf (1).pdf")}
+                    download="M.Qasim_GHL_CV.pdf (1).pdf"
                     className="download-link-button"
                   >
                     <Button text="Download my resume" />
